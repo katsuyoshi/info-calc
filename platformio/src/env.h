82c92925956e213ffc92d7bba4cac78d3320e231
Katsuyoshi Ito <kito@itosoft.com>
2023-08-19 01:56:29 +0900

    rename a folder

---
+++ platformio/src/env.h
@@ -0,0 +1,7 @@
+#if 0
+const char* ssid     = "ITO SOFT DESIGN2";
+const char* password = "isd2dayoon";
+#else
+const char* ssid     = "ITO SOFT DESIGN";
+const char* password = "isddayoon";
+#endif
